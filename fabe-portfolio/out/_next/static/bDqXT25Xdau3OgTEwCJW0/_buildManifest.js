7370f944b03390e44b7ef29789d5c161842aac07
Fabe Z <zengjiayifabe@outlook.com>
2025-12-10 03:17:46 -0500

    build new pages

--- fabe-portfolio/out/_next/static/bDqXT25Xdau3OgTEwCJW0/_buildManifest.js
+++ fabe-portfolio/out/_next/static/bDqXT25Xdau3OgTEwCJW0/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/fabe-portfolio//_next/:path+",
-        "destination": "/fabe-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
