70b8c9844cda6b26264e900297a8c1c76a7427dc
Fabe Z <zengjiayifabe@outlook.com>
2025-12-10 02:10:41 -0500

    changed file name

--- fabe-portfolio/components/Home.tsx
+++ fabe-portfolio/components/Home.tsx
@@ -4,7 +4,7 @@ import Experience from "./Experience";
 import Footer from "./Footer";
 import Header from "./Header";
 import Projects from "./Projects";
-import Skills from "./skills";
+import Skills from "./Skillstemp";
 
 export default function HomePage() {
     return (
