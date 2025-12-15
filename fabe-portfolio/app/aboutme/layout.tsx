550d0e9da99ed49847945a32cad0231d4d0d39dd
Fabe Z <zengjiayifabe@outlook.com>
2025-12-14 23:28:46 -0500

    added active button style

--- fabe-portfolio/app/aboutme/layout.tsx
+++ fabe-portfolio/app/aboutme/layout.tsx
@@ -1,24 +1,33 @@
-
+'use client';
 import Link from 'next/link';
 import styles from '../../components/styles/aboutme.module.css';
+import { usePathname } from 'next/navigation';
+
+const links = [
+  { href: '/aboutme/education', label: 'Education' },
+  { href: '/aboutme/skills', label: 'Skills' },
+  { href: '/aboutme/experience', label: 'Experience' },
+];
 
 export default function AboutMeLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname();
+
   return (
     <div className={styles.container}>
         <div className={styles.buttonDivider}>
-            <Link href="./" className={styles.buttonLink}>
-                Education
+          {links.map(({ href , label}) => (
+            <Link 
+              key={href} 
+              href={href} 
+              className={`${styles.buttonLink} ${pathname === href ? styles.active : ''}`}
+            >
+              {label}
             </Link>
-            <Link href="/aboutme/skills" className={styles.buttonLink}>
-                Skills
-            </Link>
-            <Link href="/aboutme/experience" className={styles.buttonLink}>
-                Experience
-            </Link>
+          ))}
         </div>
         <div className={styles.childrenDivider}>{children}</div>
     </div>
